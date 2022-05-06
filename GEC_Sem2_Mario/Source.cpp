2ee3157b9d5ddc3ac52704f912cc9c626788b4c0
StaffsBen <w012422l@student.staffs.ac.uk>
2022-05-06 18:06:51 +0100

    Title Screen done
    Integrated GameScreenMenu.cpp into Source.cpp to allow level changing from the main menu, via Mario.

--- GEC_Sem2_Mario/Source.cpp
+++ GEC_Sem2_Mario/Source.cpp
@@ -7,6 +7,12 @@
 #include <iostream>
 #include "GameScreenManager.h"
 
+//from GameScreenMenu
+#include "GameScreenMenu.h"
+#include "GameScreenLevel1.h"
+#include "Collisions.h"
+#include "PowBlock.h"
+
 using namespace std;
 
 //Global vars
@@ -170,3 +176,127 @@ void Render() {
 	//updates the screen
 	SDL_RenderPresent(g_renderer);
 }
+
+//from GameScreenMenu
+//GameScreenMenu::GameScreenMenu(SDL_Renderer* renderer) : GameScreen(renderer) {
+//
+//	SetUpLevel();
+//
+//	SetUpMusic();
+//}
+//
+//GameScreenMenu::~GameScreenMenu() {
+//
+//	delete m_background_texture;
+//	m_background_texture = nullptr;
+//
+//	delete m_musicplayer;
+//	m_musicplayer = nullptr;
+//
+//	delete _charMario;
+//	_charMario = nullptr;
+//
+//	m_textChars.clear();
+//}
+//
+//void GameScreenMenu::Update(float _deltaTime, SDL_Event e) {
+//
+//	_charMario->Update(_deltaTime, e);
+//
+//	if (_charMario->GetPosition().x < 140 && _charMario->GetPosition().y < 300) {
+//
+//		game_screen_manager->ChangeScreen(SCREEN_LEVEL1);
+//	}
+//}
+//
+//void GameScreenMenu::Render() {
+//
+//	//draw the background
+//	m_background_texture->Render(Vector2D(0, m_background_yPos), SDL_FLIP_NONE);
+//
+//	//draw mario
+//	_charMario->Render();
+//
+//	//draw text
+//	for (int i = 0; i < m_textChars.size(); i++)
+//		m_textChars[i]->Render();
+//}
+//
+//bool GameScreenMenu::SetUpLevel() {
+//
+//	//load texture
+//	m_background_texture = new Texture2D(m_renderer);
+//
+//	if (!m_background_texture->LoadFromFile("Images/BackgroundMB_Lv2.png")) {
+//
+//		std::cout << "Failed to load background texture! Error: " << SDL_GetError() << std::endl;
+//
+//		return false;
+//	}
+//
+//	SetLevelMap();
+//
+//	_charMario = new CharacterMario(m_renderer, _spriteSheet, Vector2D(240, 275), m_level_map);
+//
+//	CreateText(_spriteSheet, Vector2D(50, 300), 'l');
+//	CreateText(_spriteSheet, Vector2D(65, 300), 'e');
+//	CreateText(_spriteSheet, Vector2D(80, 300), 'v');
+//	CreateText(_spriteSheet, Vector2D(95, 300), 'e');
+//	CreateText(_spriteSheet, Vector2D(110, 300), 'l');
+//	CreateText(_spriteSheet, Vector2D(140, 300), '1');
+//
+//	CreateText(_spriteSheet, Vector2D(350, 300), 'l');
+//	CreateText(_spriteSheet, Vector2D(365, 300), 'e');
+//	CreateText(_spriteSheet, Vector2D(380, 300), 'v');
+//	CreateText(_spriteSheet, Vector2D(395, 300), 'e');
+//	CreateText(_spriteSheet, Vector2D(410, 300), 'l');
+//	CreateText(_spriteSheet, Vector2D(440, 300), '2');
+//
+//	m_background_yPos = 0.0f;
+//
+//	return true;
+//}
+//
+//void GameScreenMenu::SetLevelMap() {
+//
+//	std::cout << "Levelmap made" << std::endl;
+//
+//	int map[MAP_HEIGHT][MAP_WIDTH] = { { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },		//modified levelmap, left platforms extended to match background
+//									   { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+//									   { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+//									   { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+//									   { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+//									   { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+//									   { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+//									   { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+//									   { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+//									   { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+//									   { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+//									   { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+//									   { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 }
+//	};
+//
+//	//clear any old maps
+//	if (m_level_map != nullptr)
+//	{
+//		delete m_level_map;
+//	}
+//
+//	//set the new one
+//	m_level_map = new LevelMap(map);
+//}
+//
+//void GameScreenMenu::SetUpMusic() {
+//
+//	m_musicplayer = new MusicPlayer();
+//	m_musicplayer->StopMusic();
+//	m_musicplayer->PlayMusic("Audio/MarioTitle.mid");
+//}
+//
+//void GameScreenMenu::CreateText(std::string texturePath, Vector2D position, char symbol) {
+//
+//	//Creates text character and pushes to text char vector
+//	_charText = new CharacterText(m_renderer, texturePath, position, nullptr, symbol);
+//
+//	m_textChars.push_back(_charText);
+//}
